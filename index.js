f3707a719433ca20fc59af490fbb579f7b8c56ee
TTatchell <tim.tatchell24@gmail.com>
2021-03-28 19:37:56 +1100

    upload

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+const driver = {
+    name: "Sam"
+}
+
+function updateDriverWithKeyAndValue(obj, key, val) {
+    let copyOfObj = {...obj};
+    copyOfObj[key] = val;
+    return copyOfObj;
+
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, val) {
+    obj[key] = val;
+    return obj
+}
+
+function deleteFromDriverByKey(obj, key) {
+    let copyofObj = {...obj};
+    delete copyofObj[key];
+    return copyofObj;
+}
+
+
+function destructivelyDeleteFromDriverByKey(obj, key) {
+    delete obj[key];
+    return obj;
+
+}
+
+
+
+
+updateDriverWithKeyAndValue(driver, 'address', '11 broad')
